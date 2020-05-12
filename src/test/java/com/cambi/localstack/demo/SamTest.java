abf05f027881f108584561ad9a5814cd40e3518d
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-05-12 11:29:16 +0200

    adds sam test

--- src/test/java/com/cambi/localstack/demo/SamTest.java
+++ src/test/java/com/cambi/localstack/demo/SamTest.java
@@ -4,9 +4,7 @@ import cloud.localstack.docker.annotation.LocalstackDockerProperties;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(locations = "/config.properties")
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 @LocalstackDockerProperties(services = {"serverless"})
 public class SamTest extends AbstractAwsTest {
